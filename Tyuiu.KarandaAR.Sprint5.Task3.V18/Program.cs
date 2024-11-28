cd92d01e328d03b0a38206c8d49ed012667136b5
KarandaAR <karandaalexandr@gmail.com>
2024-11-28 21:35:51 +0500

    task3

---
+++ Tyuiu.KarandaAR.Sprint5.Task3.V18/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KarandaAR.Sprint5.Task3.V18.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint5.Task3.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+
+            // Оформление вывода
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Работа с файлами                                                  *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 2.                       *");
+            Console.WriteLine("* Результат сохранить в бинарный файл OutPutFileTask3.bin и вывести на    *");
+            Console.WriteLine("* консоль. Округлить до трёх знаков после запятой.                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+            Console.WriteLine("****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
