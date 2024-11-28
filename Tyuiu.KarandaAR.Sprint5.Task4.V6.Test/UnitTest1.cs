3d790a141ba863c0efcd12f315a38832afe07c4a
KarandaAR <karandaalexandr@gmail.com>
2024-11-28 21:47:10 +0500

    task 4

---
+++ Tyuiu.KarandaAR.Sprint5.Task4.V6.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KarandaAR.Sprint5.Task4.V6.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KarandaAR.Sprint5.Task4.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KarandaAR.Sprint5.Task4.V6.Lib;
+
+namespace Tyuiu.KarandaAR.Sprint5.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Спринт #5 | Выполнил: Каранда А. Р. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Работа с файлами                                                  *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Каранда Александр Романович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл C:\\DataSprint5\\InPutDataFileTask4V0.txt                       *");
+            Console.WriteLine("* (файл взять из архива согласно вашему варианту).                        *");
+            Console.WriteLine("* Создать папку вручную C:\\DataSprint5\\ и скопировать в неё файл.        *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо X в формуле.            *");
+            Console.WriteLine("* Вычислить значение по формуле.                                          *");
+            Console.WriteLine("* Полученное значение округлить до трёх знаков после запятой.             *");
+            Console.WriteLine("* Вернуть полученный результат на консоль.                                *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+            Console.WriteLine("Данные находятся в файле : " + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
