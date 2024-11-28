9fab184a11949d8aaf8c951e37cfabdc64424785
KarandaAR <karandaalexandr@gmail.com>
2024-11-28 22:50:51 +0500

    task 5

--- Tyuiu.KarandaAR.Sprint5.Task5.V15.Lib/DataService.cs
+++ Tyuiu.KarandaAR.Sprint5.Task5.V15.Lib/DataService.cs
@@ -12,29 +12,45 @@ namespace Tyuiu.KarandaAR.Sprint5.Task5.V15.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            // Чтение данных из файла
-            string[] lines = File.ReadAllLines(path);
+            try
+            {
+                // Чтение всех строк из файла
+                string[] lines = File.ReadAllLines(path);
 
-            // Преобразуем все строки в массив чисел (вещественные числа)
-            var numbers = lines
-                .SelectMany(line => line.Split(new char[] { ' ', '\t', ',' }, StringSplitOptions.RemoveEmptyEntries)) // Разделение по пробелам, табуляции и запятой
-                .Select(str =>
+                // Преобразуем все строки в массив чисел (вещественные числа)
+                var numbers = lines
+                    .SelectMany(line => line.Split(new char[] { ' ', '\t', ',', ';' }, StringSplitOptions.RemoveEmptyEntries)) // Разделяем по пробелам, табуляции, запятой, точке с запятой
+                    .Select(str =>
+                    {
+                        double num;
+                        return double.TryParse(str, out num) ? num : (double?)null; // Преобразуем строку в число
+                    })
+                    .Where(num => num.HasValue) // Убираем null-значения
+                    .Select(num => num.Value)
+                    .ToList();
+
+                // Ищем минимальное число, которое делится на 5
+                var minDivisibleByFive = numbers
+                    .Where(num => num % 5 == 0) // Фильтруем числа, которые делятся на 5
+                    .OrderBy(num => num) // Сортируем по возрастанию
+                    .FirstOrDefault(); // Берем первое минимальное (или 0, если нет)
+
+                // Если число найдено, округляем до 3 знаков после запятой и возвращаем его
+                if (minDivisibleByFive != 0)
                 {
-                    double num;
-                    return double.TryParse(str, out num) ? num : (double?)null;
-                })
-                .Where(num => num.HasValue)
-                .Select(num => num.Value)
-                .ToList();
-
-            // Ищем минимальное число, которое делится на 5
-            var minDivisibleByFive = numbers
-            .Where(num => num % 5 == 0) // Фильтруем числа, которые делятся на 5
-            .OrderBy(num => num) // Сортируем по возрастанию
-            .FirstOrDefault(); // Берем первое минимальное
-
-            return minDivisibleByFive;
-
+                    return Math.Round(minDivisibleByFive, 3);
+                }
+                else
+                {
+                    // Если числа, которые делятся на 5, нет, возвращаем -1 или какое-то другое значение
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при обработке файла: {ex.Message}");
+                return 0;
+            }
         }
     }
 }
